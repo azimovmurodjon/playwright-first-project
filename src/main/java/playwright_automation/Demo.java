849f3552c7ca3083a379a341bb2a71d67bba3f3b
M.J. Azimov <azimovm.j@gmail.com>
2022-11-07 00:47:29 -0500

    Updated Main method

--- src/main/java/playwright_automation/Demo.java
+++ src/main/java/playwright_automation/Demo.java
@@ -10,15 +10,16 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		Playwright playwright = Playwright.create();
-		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
 		Page page = browser.newPage();
 		page.navigate("https://www.google.com/");
+		System.out.println(page.title() );
 		page.type("input[name='q']", "Headphones");
 		page.click("//input[@class='gNO89b']");
 		page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
 		browser.close();
 		playwright.close();
-		
+
 	}
 
 }
